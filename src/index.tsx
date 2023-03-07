8b02e05b9004745d32b31900ae113898eb90fce9
Xavier Fabregat <xavi.fabregat.pous@gmail.com>
2023-03-08 00:13:35 +0100

    Feat: Add drawer router

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  createDrawerNavigator
+} from '@react-navigation/drawer'
+import MainScreen from './screens/main-screen';
+
+const Drawer = createDrawerNavigator();
+
+const App = () => {
+  return (
+    <Drawer.Navigator initialRouteName='Main'>
+      <Drawer.Screen name='Main' component={MainScreen} />
+    </Drawer.Navigator>
+  )
+};
+
+export default App;
