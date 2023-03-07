573f102128b37d24f1c579f964e4bb7e1330b6a4
Xavier Fabregat <xavi.fabregat.pous@gmail.com>
2023-03-07 23:01:39 +0100

    Feat: add checkbox animated

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -6,10 +6,13 @@ export default function ThemeToggle() {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
+    <>
     <HStack space={2} alignItems="center">
       <Text>Dark</Text>
       <Switch isChecked={colorMode === 'light'}  onToggle={toggleColorMode}/>
       <Text>Light</Text>
     </HStack>
+    </>
+
   )
 }
--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useCallback, useState } from 'react';
 import {
   Text,
   Box,
@@ -6,9 +6,19 @@ import {
   VStack,
   useColorModeValue,
 } from 'native-base';
+import AnimatedCheckbox from 'react-native-checkbox-reanimated';
 import ThemeToggle from '../components/theme-toggle';
+import { Pressable } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 export default function MainScreen () {
+
+  const [checked, isChecked] = React.useState<boolean>(false);
+
+  const handleCheckboxPress = () => {
+    isChecked(prev => !prev);
+  }
+
   return (
     <Center 
     _dark={{ bg: 'blueGray.900' }} 
@@ -17,6 +27,14 @@ export default function MainScreen () {
     flex={1}
     >
       <VStack space={5} alignItems="center">
+        <Pressable onPress={handleCheckboxPress} style={styles.checkbox}>
+          <AnimatedCheckbox
+            checked={checked}
+            highlightColor="#4444ff"
+            checkmarkColor='white'
+            boxOutlineColor='#4444ff'
+          />
+        </Pressable>
         <Box p={10} bg={useColorModeValue('red.500', 'yellow.500')}>
           <Text>Hello</Text>
         </Box>
@@ -24,4 +42,16 @@ export default function MainScreen () {
       </VStack>
     </Center>
   )
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  checkbox: {
+    width: 64,
+    height: 64,
+  },
+});
