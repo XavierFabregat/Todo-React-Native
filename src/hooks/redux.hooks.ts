ef622f41ff0965d6f95a33824054e773134a91d2
Xavier Fabregat <xavi.fabregat.pous@gmail.com>
2023-03-09 16:29:46 +0100

    Feat: Add custom hooks

--- src/hooks/redux.hooks.ts
+++ src/hooks/redux.hooks.ts
@@ -4,3 +4,5 @@ import type { TypedUseSelectorHook } from 'react-redux';
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export const useUser = () => useAppSelector(state => state.user);
