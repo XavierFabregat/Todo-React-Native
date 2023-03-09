041d993b3d1bdd987309da8fc6569884f5ca3315
Xavier Fabregat <xavi.fabregat.pous@gmail.com>
2023-03-09 16:30:36 +0100

    Feat: add flexibilty to theme toggle comp

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
-import { Text, HStack, Switch, useColorMode } from 'native-base';
+import { Text, HStack, Switch, useColorMode, Pressable, Icon } from 'native-base';
+import { Feather } from '@expo/vector-icons';
 
-export default function ThemeToggle() {
+interface Props {
+  mode?: string;
+}
+
+export default function ThemeToggle({ mode = "toggle" }) {
 
   const { colorMode, toggleColorMode } = useColorMode();
 
-  return (
+  return mode === "toggle" ? (
     <>
     <HStack space={2} alignItems="center">
-      <Text>Dark</Text>
+      <Icon as={<Feather name={"moon"}/>} size="lg" color={colorMode === "light" ? 'black' : 'white'} />
       <Switch isChecked={colorMode === 'light'}  onToggle={toggleColorMode}/>
-      <Text>Light</Text>
+      <Icon as={<Feather name={"sun"}/>} size="lg" color={colorMode === "light" ? 'black' : 'white'} />
     </HStack>
     </>
-
+  ) : (
+    <>
+    <HStack space={2} alignItems="center">
+      <Pressable onPress={toggleColorMode}>
+          <Icon as={<Feather name={colorMode === 'light' ? 'moon' : 'sun'}  />} size="lg" color={colorMode === "light" ? 'black' : 'white'} />
+      </Pressable>
+    </HStack>
+    </>
   )
 }
